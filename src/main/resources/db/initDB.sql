cadd7d41c2c0b5fcc7416fa96f660ad94688efa1
Uziki <uzik72@gmail.com>
2019-03-11 23:41:12 +0300

    HW03 Optional add meals unique user datetime index

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -34,3 +34,4 @@ CREATE TABLE meals
   calories      INTEGER                   NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals (user_id, date_time);
