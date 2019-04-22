cfcda2e36e8f0bf7c47d8853ca051fa8ea69a274
Uziki <uzik72@gmail.com>
2019-04-23 01:10:25 +0300

    HW09 implement 2

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -42,7 +42,7 @@ public class Meal extends AbstractBaseEntity {
     @Column(name = "calories", nullable = false)
     @Range(min = 10, max = 5000)
     @NotNull
-    private int calories;
+    private Integer calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
@@ -91,7 +91,7 @@ public class Meal extends AbstractBaseEntity {
         this.description = description;
     }
 
-    public void setCalories(int calories) {
+    public void setCalories(Integer calories) {
         this.calories = calories;
     }
 
